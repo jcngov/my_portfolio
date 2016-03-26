28498fa564f093474cd68406cca4164f87cbc96b
Jerry Ngov <jcngov@yahoo.com>
2016-03-25 22:37:34 -0700

    make states for work and contact info, and link states using ui-sref

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -13,7 +13,15 @@
       .state('home', {
         url: '/',
         templateUrl: '/js/app/layouts/home.html'
-      });
+      })
+      .state('work', {
+        url: '/work',
+        templateUrl: '/js/app/layouts/work.html'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: '/js/app/layouts/contact.html'
+      })
 
     $urlRouterProvider.otherwise('/');
   }
