d7ca690a0f5cde9375e4919d451ed123ba1ef39c
Jerry Ngov <jcngov@yahoo.com>
2016-04-11 11:07:11 -0700

    delete unecessary folders and files

--- config/environment.js
+++
@@ -1,9 +0,0 @@
-var _ = require('lodash');
-
-var localEnvVars = {
-  TITLE:      'my_portfolio',
-  SAFE_TITLE: 'my_portfolio'
-};
-
-// Merge all environmental variables into one object.
-module.exports = _.extend(process.env, localEnvVars);
